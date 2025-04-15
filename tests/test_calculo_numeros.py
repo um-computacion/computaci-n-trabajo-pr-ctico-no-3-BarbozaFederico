62eed5a2c7be1654624043ab3d2d5d907e83a21a
BarbozaFederico <f.barboza@alumno.um.edu.ar>
2025-04-15 16:49:19 -0300

    Agregar tests para ingreso de números válidos y creacion de funcion esqueleto

---
+++ tests/test_calculo_numeros.py
@@ -0,0 +1,18 @@
+import unittest
+def ingrese_numero():...
+
+#from src.exceptions import (
+#    ingrese_numero,
+#    NumeroDebeSerPositivo,
+
+from unittest.mock import patch
+
+class TestCalculoNumeros(unittest.TestCase):
+
+    @patch(  # este patch controla lo que hace el input
+        'builtins.input',
+        return_value='100'
+    )
+    def test_ingreso_feliz(self, patch_input):
+        numero = ingrese_numero()
+        self.assertEqual(numero, 100)
